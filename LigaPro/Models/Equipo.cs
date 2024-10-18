fe876c8e832badd10d3e179d92b69d3bfbf8bf70
Santi7870 <santiago.panchi@udla.edu.ec>
2024-10-18 09:02:40 -0500

    Commit 2

---
+++ LigaPro/Models/Equipo.cs
@@ -0,0 +1,13 @@
+﻿namespace LigaPro.Models
+{
+    public class Equipo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Ciudad { get; set; }
+        public string Titulos { get; set; }
+
+        public string AceptaExtranjeros {  get; set; }
+       
+    }
+}
--- LigaPro/Models/Jugadores.cs
+++ LigaPro/Models/Jugadores.cs
@@ -1,7 +1,19 @@
-﻿namespace LigaPro.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LigaPro.Models
 {
     public class Jugadores
     {
        public int Id { get; set; }
+        [MaxLength(100)]
+
+        public string Nombre { get; set; }
+        public string Posicion { get; set; }
+        public Equipo equipo { get; set; }
+        [ForeignKey("nombre")]
+        public string Edad { get; set; }
+
+
     }
 }
